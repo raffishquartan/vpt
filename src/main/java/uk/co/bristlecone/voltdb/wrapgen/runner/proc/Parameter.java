47ae2caf4262a9964609b0614498975e3d15299c
Christo Fogelberg <cfogelberg@gmail.com>
2016-12-02 21:29:55 +0000

    Initial stored proc meta data

---
+++ src/main/java/uk/co/bristlecone/voltdb/wrapgen/runner/proc/Parameter.java
@@ -0,0 +1,67 @@
+package uk.co.bristlecone.voltdb.wrapgen.runner.proc;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+/**
+ * Represents a parameter (e.g. <code>String foo</code>) to a stored procedure
+ * 
+ * @author christo
+ */
+public class Parameter {
+  private String type;
+  private String name;
+
+  private Parameter(String type, String name) {
+    this.type = type;
+    this.name = name;
+  }
+
+  public static Parameter of(String type, String name) {
+    checkArgument(type != null, "type parameter must not be null");
+    checkArgument(name != null, "name parameter must not be null");
+    checkArgument(!type.equals(""), "type parameter must not be an empty String");
+    checkArgument(!name.equals(""), "name parameter must not be an empty String");
+
+    return new Parameter(type, name);
+  }
+
+  public String type() {
+    return type;
+  }
+
+  public String name() {
+    return name;
+  }
+
+  public int hashCode() {
+    return new HashCodeBuilder(17, 37).append(type)
+        .append(name)
+        .toHashCode();
+  }
+
+  public boolean equals(Object other) {
+    if(other == null) {
+      return false;
+    }
+    if(other == this) {
+      return true;
+    }
+    if(getClass() != other.getClass()) {
+      return false;
+    }
+    Parameter rhs = (Parameter) other;
+    return new EqualsBuilder().append(type, rhs.type())
+        .append(name, rhs.name())
+        .isEquals();
+  }
+
+  public String toString() {
+    return new ToStringBuilder(this).append(type)
+        .append(name)
+        .build();
+  }
+}
---
+++ src/main/java/uk/co/bristlecone/voltdb/wrapgen/runner/proc/ProcReturnType.java
@@ -0,0 +1,11 @@
+package uk.co.bristlecone.voltdb.wrapgen.runner.proc;
+
+/**
+ * Represents the types that may be returned by a VoltDB stored procedure's run method. See the VoltDB documentation for
+ * more information: "5.2.5. Returning Results from a Stored Procedure"
+ * 
+ * @author christo
+ */
+public enum ProcReturnType {
+  LONG_PRIMITIVE, SINGLE_VOLTTABLE, VOLTABLE_ARRAY
+}
