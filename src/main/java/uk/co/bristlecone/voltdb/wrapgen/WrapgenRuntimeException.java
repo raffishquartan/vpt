3471be560bdf28aebe1ef2d6468fae5c2dd8461c
Christo Fogelberg <cfogelberg@gmail.com>
2016-12-06 06:48:22 +0000

    WrapgenRuntimeException

---
+++ src/main/java/uk/co/bristlecone/voltdb/wrapgen/WrapgenRuntimeException.java
@@ -0,0 +1,13 @@
+package uk.co.bristlecone.voltdb.wrapgen;
+
+/**
+ * A catch-all (haha) exception class for runtime errors in WrapGen.
+ * 
+ * @author christo
+ */
+public class WrapgenRuntimeException extends RuntimeException {
+  /**
+   * serialVersionUID not used
+   */
+  private static final long serialVersionUID = 1L;
+}
