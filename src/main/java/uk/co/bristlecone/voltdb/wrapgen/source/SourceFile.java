c6d244e6a45d25ea8c85c3a439da134e481569e2
Christo Fogelberg <cfogelberg@gmail.com>
2016-12-06 06:48:04 +0000

    SourceFile refactored to interface, JavaParserSourceFile stubbed as impl

--- src/main/java/uk/co/bristlecone/voltdb/wrapgen/source/SourceFile.java
+++ src/main/java/uk/co/bristlecone/voltdb/wrapgen/source/SourceFile.java
@@ -1,5 +1,40 @@
 package uk.co.bristlecone.voltdb.wrapgen.source;
 
-public class SourceFile {
+import java.util.List;
 
+import uk.co.bristlecone.voltdb.wrapgen.VoltRunner;
+import uk.co.bristlecone.voltdb.wrapgen.WrapgenRuntimeException;
+
+/**
+ * Represents a Java source file and provides accessor methods for extracting metadata needed to build a
+ * {@link VoltRunner}. These accessor methods may throw a {@link WrapgenRuntimeException} if the associated Java source
+ * does not contain a valid VoltDB stored procedure, so the {@link SourceFile#isValidVoltProcedure} method should be
+ * used to guard against this.
+ * 
+ * @author christo
+ */
+public interface SourceFile {
+  /**
+   * @return true iff this SourceFile contains a valid VoltDB stored procedure
+   */
+  boolean isValidVoltProcedure();
+
+  /**
+   * @return the name of the VoltDB stored procedure in this source file; may throw a {@link WrapgenRuntimeException} if
+   *         the associated Java source does not contain a valid VoltDB stored procedure
+   */
+  String voltProcedureName();
+
+  /**
+   * @return the parameters to the VoltDB stored procedure's run method; may throw a {@link WrapgenRuntimeException} if
+   *         the associated Java source does not contain a valid VoltDB stored procedure
+   */
+  List<Parameter> runMethodParameters();
+
+  /**
+   * @return the return type of the VoltDB stored procedure in this source file, may throw a
+   *         {@link WrapgenRuntimeException} if the associated Java source does not contain a valid VoltDB stored
+   *         procedure
+   */
+  ProcReturnType runMethodReturnType();
 }
---
+++ src/main/java/uk/co/bristlecone/voltdb/wrapgen/source/impl/JavaParserSourceFile.java
@@ -0,0 +1,64 @@
+package uk.co.bristlecone.voltdb.wrapgen.source.impl;
+
+import java.util.List;
+
+import com.github.javaparser.ast.CompilationUnit;
+
+import uk.co.bristlecone.voltdb.wrapgen.source.Parameter;
+import uk.co.bristlecone.voltdb.wrapgen.source.ProcReturnType;
+import uk.co.bristlecone.voltdb.wrapgen.source.SourceFile;
+
+/**
+ * Implements the {@link SourceFile} interface using a <code>com.github.javaparser.ast.CompilationUnit</code>.
+ * 
+ * @author christo
+ */
+public class JavaParserSourceFile implements SourceFile {
+  public JavaParserSourceFile(CompilationUnit ast) {
+
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see uk.co.bristlecone.voltdb.wrapgen.source.SourceFile#isValidVoltProcedure()
+   */
+  @Override
+  public boolean isValidVoltProcedure() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see uk.co.bristlecone.voltdb.wrapgen.source.SourceFile#voltProcedureName()
+   */
+  @Override
+  public String voltProcedureName() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see uk.co.bristlecone.voltdb.wrapgen.source.SourceFile#runMethodParameters()
+   */
+  @Override
+  public List<Parameter> runMethodParameters() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see uk.co.bristlecone.voltdb.wrapgen.source.SourceFile#runMethodReturnType()
+   */
+  @Override
+  public ProcReturnType runMethodReturnType() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+}
