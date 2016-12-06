7eaa75cf84063fc5a786531f95c4eea45402e742
Christo Fogelberg <cfogelberg@gmail.com>
2016-12-06 21:09:53 +0000

    implement JavaparserSourceFile

--- src/main/java/uk/co/bristlecone/voltdb/wrapgen/source/impl/JavaparserSourceFile.java
+++ src/main/java/uk/co/bristlecone/voltdb/wrapgen/source/impl/JavaparserSourceFile.java
@@ -1,11 +1,19 @@
 package uk.co.bristlecone.voltdb.wrapgen.source.impl;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+import com.github.javaparser.printer.PrettyPrinterConfiguration;
 
-import uk.co.bristlecone.voltdb.wrapgen.source.RunParameter;
+import uk.co.bristlecone.voltdb.wrapgen.WrapgenRuntimeException;
 import uk.co.bristlecone.voltdb.wrapgen.source.ProcReturnType;
+import uk.co.bristlecone.voltdb.wrapgen.source.RunParameter;
 import uk.co.bristlecone.voltdb.wrapgen.source.SourceFile;
 
 /**
@@ -13,9 +21,13 @@ import uk.co.bristlecone.voltdb.wrapgen.source.SourceFile;
  * 
  * @author christo
  */
-public class JavaParserSourceFile implements SourceFile {
-  public JavaParserSourceFile(CompilationUnit ast) {
+public class JavaparserSourceFile implements SourceFile {
+  private CompilationUnit ast;
+  private String filepath;
 
+  public JavaparserSourceFile(CompilationUnit ast, String filepath) {
+    this.ast = ast;
+    this.filepath = filepath;
   }
 
   /*
@@ -25,8 +37,7 @@ public class JavaParserSourceFile implements SourceFile {
    */
   @Override
   public boolean isValidVoltProcedure() {
-    // TODO Auto-generated method stub
-    return false;
+    throw new WrapgenRuntimeException("Error NYI");
   }
 
   /*
@@ -36,8 +47,10 @@ public class JavaParserSourceFile implements SourceFile {
    */
   @Override
   public String voltProcedureName() {
-    // TODO Auto-generated method stub
-    return null;
+    return getClassExtendingVoltProcedure().map(c -> c.getName())
+        .map(sn -> sn.getIdentifier())
+        .orElseThrow(
+            () -> new WrapgenRuntimeException(String.format("No VoltProcedure-extending type found in %s", filepath)));
   }
 
   /*
@@ -46,9 +59,13 @@ public class JavaParserSourceFile implements SourceFile {
    * @see uk.co.bristlecone.voltdb.wrapgen.source.SourceFile#runMethodParameters()
    */
   @Override
-  public List<Parameter> runMethodParameters() {
-    // TODO Auto-generated method stub
-    return null;
+  public List<RunParameter> runMethodParameters() {
+    return getRunMethod().map(m -> m.getParameters())
+        .map(pl -> pl.stream()
+            .map(p -> RunParameter.of(javaparserParameterToType(p), javaparserParameterToName(p))))
+        .map(s -> s.collect(Collectors.toList()))
+        .orElseThrow(() -> new WrapgenRuntimeException(
+            String.format("Either no VoltProcedure-extending type found in %s, or no run method defined", filepath)));
   }
 
   /*
@@ -58,7 +75,53 @@ public class JavaParserSourceFile implements SourceFile {
    */
   @Override
   public ProcReturnType runMethodReturnType() {
-    // TODO Auto-generated method stub
-    return null;
+    return getRunMethod().map(m -> m.getElementType())
+        .map(e -> e.toString(new PrettyPrinterConfiguration().setPrintComments(false)))
+        .map(ProcReturnType::parseJavaType)
+        .orElseThrow(() -> new WrapgenRuntimeException(
+            String.format("Either no VoltProcedure-extending type found in %s, or no run method defined", filepath)));
+  }
+
+  /**
+   * @return the first {@link ClassOrInterfaceDeclaration} type in this source file which extends a class called
+   *         "VoltProcedure" (NB: Does not do full symbol resolution, so this method could be misled by another class of
+   *         the same name in a different package).
+   */
+  private Optional<ClassOrInterfaceDeclaration> getClassExtendingVoltProcedure() {
+    ClassOrInterfaceType voltProcedure = new ClassOrInterfaceType("VoltProcedure");
+    return ast.getTypes()
+        .stream()
+        .filter(d -> d instanceof ClassOrInterfaceDeclaration)
+        .map(d -> (ClassOrInterfaceDeclaration) d)
+        .filter(d -> d.getExtends()
+            .contains(voltProcedure))
+        .findFirst();
+  }
+
+  /**
+   * @return the first method overload for "run" in the class returned by
+   *         {@link JavaparserSourceFile#getClassExtendingVoltProcedure}
+   */
+  private Optional<MethodDeclaration> getRunMethod() {
+    return getClassExtendingVoltProcedure().flatMap(c -> c.getMethodsByName("run")
+        .stream()
+        .findFirst());
+  }
+
+  /**
+   * @param param a JavaParser Parameter
+   * @return the type of <code>param</code>, as a String
+   */
+  private static String javaparserParameterToType(Parameter param) {
+    return param.getType()
+        .toString();
+  }
+
+  /**
+   * @param param a JavaParser Parameter
+   * @return the variable name of <code>param</code>, as a String
+   */
+  private static String javaparserParameterToName(Parameter param) {
+    return param.getNameAsString();
   }
 }
