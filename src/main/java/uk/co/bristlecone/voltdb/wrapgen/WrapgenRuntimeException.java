02b5823be42e0c4f8cfd4a2937682cdd1c8308c0
Christo Fogelberg <cfogelberg@gmail.com>
2016-12-06 18:42:12 +0000

    WrapgenRuntimeException - add standard exception ctors

--- src/main/java/uk/co/bristlecone/voltdb/wrapgen/WrapgenRuntimeException.java
+++ src/main/java/uk/co/bristlecone/voltdb/wrapgen/WrapgenRuntimeException.java
@@ -10,4 +10,20 @@ public class WrapgenRuntimeException extends RuntimeException {
    * serialVersionUID not used
    */
   private static final long serialVersionUID = 1L;
+
+  public WrapgenRuntimeException() {
+    super();
+  }
+
+  public WrapgenRuntimeException(String message) {
+    super(message);
+  }
+
+  public WrapgenRuntimeException(Throwable t) {
+    super(t);
+  }
+
+  public WrapgenRuntimeException(String message, Throwable t) {
+    super(message, t);
+  }
 }
