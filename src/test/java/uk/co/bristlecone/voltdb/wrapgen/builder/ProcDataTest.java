58108cafd673af8ba27483d0fcf742ef07b462c9
Christo Fogelberg <cfogelberg@gmail.com>
2016-12-22 20:23:57 +0000

    test packageName, setPackageName in ProcData builder

--- src/test/java/uk/co/bristlecone/voltdb/wrapgen/builder/ProcDataTest.java
+++ src/test/java/uk/co/bristlecone/voltdb/wrapgen/builder/ProcDataTest.java
@@ -10,24 +10,26 @@ import org.junit.Test;
 
 import com.google.common.collect.ImmutableList;
 
-import uk.co.bristlecone.voltdb.wrapgen.source.RunParameter;
-import uk.co.bristlecone.voltdb.wrapgen.builder.ProcData;
 import uk.co.bristlecone.voltdb.wrapgen.source.ProcReturnType;
+import uk.co.bristlecone.voltdb.wrapgen.source.RunParameter;
 
 public class ProcDataTest {
   private final static String TEST_NAME = "testName";
   private final static List<RunParameter> TEST_PARAMETERS = ImmutableList.of(RunParameter.of("testType", "testName"));
   private final static ProcReturnType TEST_RETURN_TYPE = ProcReturnType.VOLTABLE_ARRAY;
+  private final static String TEST_PACKAGE_NAME = "test.package";
 
   @Test
   public void builderInstantiatesClassCorrectly() {
     ProcData testee = new ProcData.Builder().setName(TEST_NAME)
         .setParameters(TEST_PARAMETERS)
         .setReturnType(TEST_RETURN_TYPE)
+        .setPackageName(TEST_PACKAGE_NAME)
         .build();
     assertThat(testee.name(), is(equalTo(TEST_NAME)));
     assertThat(testee.parameters(), is(equalTo(TEST_PARAMETERS)));
     assertThat(testee.returnType(), is(equalTo(TEST_RETURN_TYPE)));
+    assertThat(testee.packageName(), is(equalTo(TEST_PACKAGE_NAME)));
   }
 
   @Test(expected = IllegalArgumentException.class)
