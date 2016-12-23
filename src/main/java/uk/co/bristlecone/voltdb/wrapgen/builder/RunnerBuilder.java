79354e84b03a0a905cb4bf77c442e428c886e911
Christo Fogelberg <cfogelberg@gmail.com>
2016-12-23 23:47:59 +0000

    RunnerBuilder - initial, partial/stub impl of class building

--- src/main/java/uk/co/bristlecone/voltdb/wrapgen/builder/RunnerBuilder.java
+++ src/main/java/uk/co/bristlecone/voltdb/wrapgen/builder/RunnerBuilder.java
@@ -2,6 +2,13 @@ package uk.co.bristlecone.voltdb.wrapgen.builder;
 
 import java.util.function.Function;
 
+import javax.lang.model.element.Modifier;
+
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.TypeSpec;
+
+import uk.co.bristlecone.voltdb.wrapgen.runner.VoltRunner;
+
 public class RunnerBuilder {
   private Function<String, String> packageNamer;
   private Function<String, String> runnerNamer;
@@ -14,6 +21,12 @@ public class RunnerBuilder {
   }
 
   public VoltRunnerJavaSource build() {
-    throw new UnsupportedOperationException("NYI");
+    TypeSpec runnerClassBuilder = TypeSpec.classBuilder(runnerNamer.apply(procData.name()))
+        .addModifiers(Modifier.PUBLIC)
+        .addAnnotation(VoltRunner.class)
+        .build();
+    JavaFile fileBuilder = JavaFile.builder(packageNamer.apply(procData.packageName()), runnerClassBuilder)
+        .build();
+    return new VoltRunnerJavaSource(fileBuilder.toString());
   }
 }
--- src/test/java/uk/co/bristlecone/voltdb/wrapgen/builder/RunnerBuilderTest.java
+++ src/test/java/uk/co/bristlecone/voltdb/wrapgen/builder/RunnerBuilderTest.java
@@ -1,5 +1,45 @@
 package uk.co.bristlecone.voltdb.wrapgen.builder;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+import java.util.function.Function;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import mockit.Expectations;
+import mockit.Mocked;
+import mockit.integration.junit4.JMockit;
+
+@RunWith(JMockit.class)
 public class RunnerBuilderTest {
 
+  // @formatter:off
+  private static final String PROC_DATA_PACKAGE_NAME = "mock.package.name";
+  private static final String PROC_DATA_CLASS_NAME = "MockStoredProcedureName";
+  private static final String EXPECTED_CLASS_WITH_IDENTITY_NAMERS = "" 
+      + String.format("package %s;\n", PROC_DATA_PACKAGE_NAME)
+      + String.format("\n")
+      + String.format("import uk.co.bristlecone.voltdb.wrapgen.runner.VoltRunner;\n")
+      + String.format("\n")
+      + String.format("@VoltRunner\n")
+      + String.format("public class %s {\n", PROC_DATA_CLASS_NAME)
+      + String.format("}\n");
+  // @formatter:on
+
+  @Test
+  public void builderCreatesClassAsExpected(@Mocked final ProcData mockProcData) {
+    // @formatter:off
+    new Expectations() {{
+      mockProcData.packageName(); result = PROC_DATA_PACKAGE_NAME;
+      mockProcData.name(); result = PROC_DATA_CLASS_NAME;
+    }};
+    // @formatter:on
+
+    VoltRunnerJavaSource testee = new RunnerBuilder(mockProcData, Function.identity(), Function.identity()).build();
+
+    assertThat(testee.source(), is(equalTo(EXPECTED_CLASS_WITH_IDENTITY_NAMERS)));
+  }
 }
