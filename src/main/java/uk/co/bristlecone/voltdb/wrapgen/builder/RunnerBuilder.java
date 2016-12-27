f17b99b36789633253a1c0efad867a75e3cb2e20
Christo Fogelberg <cfogelberg@gmail.com>
2016-12-27 20:41:18 +0000

    Refactor RunnerBuilder to use RunnerBuilderServices; add class javadoc to RunnerBuilder test

--- src/main/java/uk/co/bristlecone/voltdb/wrapgen/builder/RunnerBuilder.java
+++ src/main/java/uk/co/bristlecone/voltdb/wrapgen/builder/RunnerBuilder.java
@@ -7,25 +7,28 @@ import javax.lang.model.element.Modifier;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.TypeSpec;
 
+import uk.co.bristlecone.voltdb.wrapgen.builder.impl.RunnerBuilderServices;
 import uk.co.bristlecone.voltdb.wrapgen.runner.VoltRunner;
 
+/**
+ * Allows Java class definitions for VoltRunner's to be easily built from ProcData and supporting metadata
+ * 
+ * @author christo
+ */
 public class RunnerBuilder {
-  private Function<String, String> packageNamer;
-  private Function<String, String> runnerNamer;
-  private ProcData procData;
+  private RunnerBuilderServices rbs;
 
   public RunnerBuilder(ProcData procData, Function<String, String> packageNamer, Function<String, String> runnerNamer) {
-    this.packageNamer = packageNamer;
-    this.runnerNamer = runnerNamer;
-    this.procData = procData;
+    rbs = new RunnerBuilderServices(procData, packageNamer, runnerNamer);
   }
 
   public VoltRunnerJavaSource build() {
-    TypeSpec runnerClassBuilder = TypeSpec.classBuilder(runnerNamer.apply(procData.name()))
+    TypeSpec runnerClassBuilder = TypeSpec.classBuilder(rbs.runnerName())
         .addModifiers(Modifier.PUBLIC)
         .addAnnotation(VoltRunner.class)
+        .addJavadoc(rbs.runnerJavaDoc())
         .build();
-    JavaFile fileBuilder = JavaFile.builder(packageNamer.apply(procData.packageName()), runnerClassBuilder)
+    JavaFile fileBuilder = JavaFile.builder(rbs.runnerPackageName(), runnerClassBuilder)
         .build();
     return new VoltRunnerJavaSource(fileBuilder.toString());
   }
