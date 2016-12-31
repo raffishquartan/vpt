7555d2e9d1ae88d3ada93c8d3207c434af7114fa
Christo Fogelberg <cfogelberg@gmail.com>
2016-12-31 12:32:49 +0000

    add test for runMethodParamsAsParameterSpecs to RunnerBuilderServicesTest, add final keywords

--- src/test/java/uk/co/bristlecone/voltdb/wrapgen/builder/impl/RunnerBuilderServicesTest.java
+++ src/test/java/uk/co/bristlecone/voltdb/wrapgen/builder/impl/RunnerBuilderServicesTest.java
@@ -1,15 +1,18 @@
 package uk.co.bristlecone.voltdb.wrapgen.builder.impl;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
+import java.util.Date;
 import java.util.function.Function;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import com.google.common.collect.ImmutableList;
+import com.squareup.javapoet.ParameterSpec;
 
 import mockit.Expectations;
 import mockit.Mocked;
@@ -27,6 +30,11 @@ public class RunnerBuilderServicesTest {
   private static Function<String, String> PACKAGE_NAMER = (procPackage) -> "runner." + procPackage;
   private static Function<String, String> RUNNER_NAMER = (procName) -> procName + "Runner";
 
+  private static final ParameterSpec[] EXP_PARAMETER_SPECS_AS_ARRAY = new ParameterSpec[] {
+      ParameterSpec.builder(Date.class, "someDate")
+          .build(),
+      ParameterSpec.builder(int.class, "someInt")
+          .build() };
   private static final String EXP_RUNNER_PACKAGE_NAME = "runner." + PROC_DATA_PACKAGE_NAME;
   private static final String EXP_RUNNER_CLASS_NAME = PROC_DATA_CLASS_NAME + "Runner";
   // @formatter:off
@@ -48,7 +56,7 @@ public class RunnerBuilderServicesTest {
       mockProcData.packageName(); result = PROC_DATA_PACKAGE_NAME;
     }};
     // @formatter:on
-    RunnerBuilderServices testee = new RunnerBuilderServices(mockProcData, PACKAGE_NAMER, RUNNER_NAMER);
+    final RunnerBuilderServices testee = new RunnerBuilderServices(mockProcData, PACKAGE_NAMER, RUNNER_NAMER);
     assertThat(testee.procPackageName(), is(equalTo(PROC_DATA_PACKAGE_NAME)));
   }
 
@@ -59,7 +67,7 @@ public class RunnerBuilderServicesTest {
       mockProcData.packageName(); result = PROC_DATA_PACKAGE_NAME;
     }};
     // @formatter:on
-    RunnerBuilderServices testee = new RunnerBuilderServices(mockProcData, PACKAGE_NAMER, RUNNER_NAMER);
+    final RunnerBuilderServices testee = new RunnerBuilderServices(mockProcData, PACKAGE_NAMER, RUNNER_NAMER);
     assertThat(testee.runnerPackageName(), is(equalTo(EXP_RUNNER_PACKAGE_NAME)));
   }
 
@@ -70,7 +78,7 @@ public class RunnerBuilderServicesTest {
       mockProcData.packageName(); result = PROC_DATA_PACKAGE_NAME;
     }};
     // @formatter:on
-    RunnerBuilderServices testee = new RunnerBuilderServices(mockProcData, PACKAGE_NAMER, RUNNER_NAMER);
+    final RunnerBuilderServices testee = new RunnerBuilderServices(mockProcData, PACKAGE_NAMER, RUNNER_NAMER);
     assertThat(testee.runnerPackageName(), is(equalTo(EXP_RUNNER_PACKAGE_NAME)));
   }
 
@@ -81,7 +89,7 @@ public class RunnerBuilderServicesTest {
       mockProcData.name(); result = PROC_DATA_CLASS_NAME;
     }};
     // @formatter:on
-    RunnerBuilderServices testee = new RunnerBuilderServices(mockProcData, PACKAGE_NAMER, RUNNER_NAMER);
+    final RunnerBuilderServices testee = new RunnerBuilderServices(mockProcData, PACKAGE_NAMER, RUNNER_NAMER);
     assertThat(testee.runnerName(), is(equalTo(EXP_RUNNER_CLASS_NAME)));
   }
 
@@ -93,7 +101,7 @@ public class RunnerBuilderServicesTest {
       mockProcData.classJavaDoc(); result = PROC_DATA_CLASS_JAVADOC;
     }};
     // @formatter:on
-    RunnerBuilderServices testee = new RunnerBuilderServices(mockProcData, PACKAGE_NAMER, RUNNER_NAMER);
+    final RunnerBuilderServices testee = new RunnerBuilderServices(mockProcData, PACKAGE_NAMER, RUNNER_NAMER);
     assertThat(testee.runnerJavaDoc(), is(equalTo(EXP_RUNNER_CLASS_JAVADOC)));
   }
 
@@ -101,11 +109,23 @@ public class RunnerBuilderServicesTest {
   public void runMethodParamsAsVariableListWorksCorrectly(@Mocked final ProcData mockProcData) {
     // @formatter:off
     new Expectations() {{
-        mockProcData.parameters(); result = ImmutableList.of(RunParameterClass.of("java.util", "Date", "someDate"), 
+        mockProcData.parameters(); result = ImmutableList.of(RunParameterClass.of("java.util", "Date", "someDate"),
             RunParameterPrimitive.ofInt("someInt"));
     }};
     // @formatter:on
-    RunnerBuilderServices testee = new RunnerBuilderServices(mockProcData, PACKAGE_NAMER, RUNNER_NAMER);
+    final RunnerBuilderServices testee = new RunnerBuilderServices(mockProcData, PACKAGE_NAMER, RUNNER_NAMER);
     assertThat(testee.runMethodParamsAsVariableList(), is(equalTo("someDate, someInt")));
+  }
+
+  @Test
+  public void runMethodParamsAsParameterSpecsReturnsExpectedParameterSpecs(@Mocked final ProcData mockProcData) {
+    // @formatter:off
+    new Expectations() {{
+        mockProcData.parameters(); result = ImmutableList.of(RunParameterClass.of("java.util", "Date", "someDate"),
+            RunParameterPrimitive.ofInt("someInt"));
+    }};
+    // @formatter:on
+    final RunnerBuilderServices testee = new RunnerBuilderServices(mockProcData, PACKAGE_NAMER, RUNNER_NAMER);
+    assertThat(testee.runMethodParamsAsParameterSpecs(), containsInAnyOrder(EXP_PARAMETER_SPECS_AS_ARRAY));
   }
 }
