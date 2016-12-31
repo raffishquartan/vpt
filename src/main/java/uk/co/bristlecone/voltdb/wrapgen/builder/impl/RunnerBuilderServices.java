39c828258cf181eb9f514fb209116b7af25b8e56
Christo Fogelberg <cfogelberg@gmail.com>
2016-12-31 12:33:24 +0000

    Fix RunnerBuilderServices#runMethodParamsAsParameterSpecs, add final keywords

--- src/main/java/uk/co/bristlecone/voltdb/wrapgen/builder/impl/RunnerBuilderServices.java
+++ src/main/java/uk/co/bristlecone/voltdb/wrapgen/builder/impl/RunnerBuilderServices.java
@@ -1,25 +1,29 @@
 package uk.co.bristlecone.voltdb.wrapgen.builder.impl;
 
+import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.ParameterSpec;
-import com.squareup.javapoet.TypeName;
 
 import uk.co.bristlecone.voltdb.wrapgen.builder.ProcData;
+import uk.co.bristlecone.voltdb.wrapgen.source.RunParameter;
+import uk.co.bristlecone.voltdb.wrapgen.source.RunParameterClass;
+import uk.co.bristlecone.voltdb.wrapgen.source.RunParameterPrimitive;
 
 /**
  * Provides methods that provide services to a RunnerBuilder, allowing it to create the full class definition.
- * 
+ *
  * @author christo
  */
 public class RunnerBuilderServices {
-  private Function<String, String> packageNamer;
-  private Function<String, String> runnerNamer;
-  private ProcData procData;
+  private final Function<String, String> packageNamer;
+  private final Function<String, String> runnerNamer;
+  private final ProcData procData;
 
-  public RunnerBuilderServices(ProcData procData, Function<String, String> packageNamer,
-      Function<String, String> runnerNamer) {
+  public RunnerBuilderServices(final ProcData procData, final Function<String, String> packageNamer,
+      final Function<String, String> runnerNamer) {
     this.packageNamer = packageNamer;
     this.runnerNamer = runnerNamer;
     this.procData = procData;
@@ -42,7 +46,7 @@ public class RunnerBuilderServices {
   }
 
   public String runnerJavaDoc() {
-    StringBuilder result = new StringBuilder();
+    final StringBuilder result = new StringBuilder();
     result.append("An instance of this class can be used to run the <code>");
     result.append(procName());
     result.append("</code> VoltDB stored procedure. This stored procedure's JavaDoc is:\n\n");
@@ -55,13 +59,15 @@ public class RunnerBuilderServices {
   }
 
   /**
-   * @return the parameters to the stored procedure's run method, converting them from RunParameter's to ParameterSpec's
+   * Returns the parameters to the stored procedure's run method, converting them from RunParameter's to
+   * ParameterSpec's. Uses {@link RunnerBuilderServices#runParameterToParameterSpecBuilder} which internally uses
+   * {@link RunParameter#isPrimitive} to dististinguish between reference/class and primitive RunParameter's. Ugly
+   * manual dispatch :/
    */
-  public Iterable<ParameterSpec> runMethodParamsAsParameterSpecs() {
+  public List<ParameterSpec> runMethodParamsAsParameterSpecs() {
     return procData.parameters()
         .stream()
-        .map(rp -> ParameterSpec.builder(TypeName.BYTE, "foo"))
-        // .map(rp -> ParameterSpec.builder(ClassName.get(rp.packageName(), rp.typeName()), rp.variableName()))
+        .map(RunnerBuilderServices::runParameterToParameterSpecBuilder)
         .map(psb -> psb.build())
         .collect(Collectors.toList());
   }
@@ -74,5 +80,16 @@ public class RunnerBuilderServices {
         .stream()
         .map(rp -> rp.variableName())
         .collect(Collectors.joining(", "));
+  }
+
+  private static ParameterSpec.Builder runParameterToParameterSpecBuilder(final RunParameter rp) {
+    if(rp.isPrimitive()) {
+      final RunParameterPrimitive rpp = (RunParameterPrimitive) rp;
+      return ParameterSpec.builder(rpp.typeClass(), rpp.variableName());
+    } else {
+      final RunParameterClass rpc = (RunParameterClass) rp;
+      return ParameterSpec.builder(ClassName.get(rpc.packageName(), rpc.typeName()), rpc.variableName());
+    }
+
   }
 }
