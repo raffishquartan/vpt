6ba86c6dde920258d6658922a3af46492cf5892b
Christo Fogelberg <cfogelberg@gmail.com>
2016-12-31 14:02:44 +0000

    add run* method javadoc expectations to RunnerBuilderTest

--- src/test/java/uk/co/bristlecone/voltdb/wrapgen/builder/RunnerBuilderTest.java
+++ src/test/java/uk/co/bristlecone/voltdb/wrapgen/builder/RunnerBuilderTest.java
@@ -19,7 +19,8 @@ public class RunnerBuilderTest {
   // @formatter:off
   private static final String PROC_DATA_PACKAGE_NAME = "mock.package.name";
   private static final String PROC_DATA_CLASS_NAME = "MockStoredProcedureName";
-  private static final String EXPECTED_CLASS_WITH_IDENTITY_NAMERS = ""
+  private static final String PROC_DATA_CLASS_JAVADOC = "Expected JavaDoc for the stored procedure class";
+  private static final String EXP_CLASS_WITH_IDENTITY_NAMERS = ""
       + String.format("package %s;\n", PROC_DATA_PACKAGE_NAME)
       + String.format("\n")
       + String.format("import java.io.IOException;\n")
@@ -36,14 +37,25 @@ public class RunnerBuilderTest {
       + String.format(" * An instance of this class can be used to run the <code>%s</code> ", PROC_DATA_CLASS_NAME)
         + String.format("VoltDB stored procedure. This stored procedure's JavaDoc is:\n")
       + String.format(" *\n")
-      + String.format(" * <blockquote>null</blockquote>\n")
+      + String.format(" * <blockquote>%s</blockquote>\n", PROC_DATA_CLASS_JAVADOC)
       + String.format(" *\n")
-      + String.format(" * <strong>This class is automatically generated. Manual edits will be overwritten.</strong>\n")
+      + String.format(" * <strong>Class is automatically generated. Manual edits will be overwritten.</strong>\n")
       + String.format(" *\n")
       + String.format(" * @author voltdb-wrapgen\n")
       + String.format(" */\n")
       + String.format("@VoltRunner\n")
       + String.format("public class %s {\n", PROC_DATA_CLASS_NAME)
+      + String.format("  /**\n")
+      + String.format("   * <strong>Class is automatically generated. Manual edits will be overwritten.</strong>\n")
+      + String.format("   *\n")
+      + String.format("   * For more information on the parameters and usage, see the underlying stored procedure: ")
+        + String.format("{@link %s.%s}\n", PROC_DATA_PACKAGE_NAME, PROC_DATA_CLASS_NAME)
+      + String.format("   *\n")
+      + String.format("   * @param client The VoltDB client that is used to execute the %s stored procedure\n",
+          PROC_DATA_CLASS_NAME)
+      + String.format("   * @return A CompletableFuture for the result of the stored procedure call\n")
+      + String.format("   * @throws when the underlying call to {@link Client#callProcedure} (or similar) throws\n")
+      + String.format("   */\n")
       + String.format("  CompletableFuture<ClientResponse> run(final Client client)\n")
       + String.format("      throws NoConnectionsException, IOException {\n")
       + String.format("    CompletableFuture<ClientResponse> result = new CompletableFuture<ClientResponse>();\n")
@@ -52,6 +64,17 @@ public class RunnerBuilderTest {
       + String.format("    return result;\n")
       + String.format("  }\n")
       + String.format("\n")
+      + String.format("  /**\n")
+      + String.format("   * <strong>Class is automatically generated. Manual edits will be overwritten.</strong>\n")
+      + String.format("   *\n")
+      + String.format("   * For more information on the parameters and usage, see the underlying stored procedure: ")
+        + String.format("{@link %s.%s}\n", PROC_DATA_PACKAGE_NAME, PROC_DATA_CLASS_NAME)
+      + String.format("   *\n")
+      + String.format("   * @param client The VoltDB client that is used to execute the %s stored procedure\n",
+          PROC_DATA_CLASS_NAME)
+      + String.format("   * @return A CompletableFuture for the result of the stored procedure call\n")
+      + String.format("   * @throws when the underlying call to {@link Client#callProcedure} (or similar) throws\n")
+      + String.format("   */\n")
       + String.format("  CompletableFuture<ClientResponse> runWithTimeout(final Client client, final Duration timeout)\n")
       + String.format("      throws NoConnectionsException, IOException {\n")
       + String.format("    CompletableFuture<ClientResponse> result = new CompletableFuture<ClientResponse>();\n")
@@ -68,12 +91,15 @@ public class RunnerBuilderTest {
     new Expectations() {{
       mockProcData.packageName(); result = PROC_DATA_PACKAGE_NAME;
       mockProcData.name(); result = PROC_DATA_CLASS_NAME;
+      mockProcData.fullyQualifiedName(); result = String.format("%s.%s", PROC_DATA_PACKAGE_NAME, PROC_DATA_CLASS_NAME);
+      mockProcData.classJavaDoc(); result = PROC_DATA_CLASS_JAVADOC;
     }};
     // @formatter:on
 
     final VoltRunnerJavaSource testee = new RunnerBuilder(mockProcData, Function.identity(), Function.identity())
         .build();
 
-    assertThat(testee.source(), is(equalToIgnoringWhiteSpace(EXPECTED_CLASS_WITH_IDENTITY_NAMERS)));
+    assertThat(testee.source(), is(equalToIgnoringWhiteSpace(EXP_CLASS_WITH_IDENTITY_NAMERS)));
+    // assertThat(testee.source(), is(equalTo(EXP_CLASS_WITH_IDENTITY_NAMERS)));
   }
 }
