38133002219ef1059edba6db3a1ae1ed276a4a8c
Christo Fogelberg <cfogelberg@gmail.com>
2017-03-06 07:02:49 +0000

    Update ConsoleWrapgenMain to use ConsoleWrapgenController, DirSourceFileProvider, SourceFileProcessor

--- console-wrapgen/src/main/java/uk/co/bristlecone/voltdb/wrapgen/console/ConsoleWrapgenMain.java
+++ console-wrapgen/src/main/java/uk/co/bristlecone/voltdb/wrapgen/console/ConsoleWrapgenMain.java
@@ -1,11 +1,14 @@
 package uk.co.bristlecone.voltdb.wrapgen.console;
 
+import uk.co.bristlecone.voltdb.wrapgen.console.impl.DirSourceFileProvider;
+
 public class ConsoleWrapgenMain {
   private ConsoleWrapgenMain() {
     // intentionally empty
   }
 
-  public static void main(String[] args) {
-    new ConsoleWrapgenController().run(args);
+  public static void main(final String[] args) {
+    final Configuration config = new Configuration(args);
+    new ConsoleWrapgenController().run(new DirSourceFileProvider(config.sourceDir()), new SourceFileProcessor(config));
   }
 }
